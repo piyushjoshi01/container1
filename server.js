0a5a835bf22fd067f37a348a851f369c94cdc1d1
Piyush Rakeshbhai Joshi <py287300@dal.ca>
2024-07-06 18:45:33 -0300

    Container-1 Source Code added

---
+++ server.js
@@ -0,0 +1,52 @@
+const { default: axios } = require("axios");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.use(express.json());
+
+const port = 3000;
+
+app.post("/store-file", async (req, res) => {
+  const { file, data } = req.body;
+
+  try {
+    if (!file) {
+      return res.status(400).json({
+        file: file,
+        error: "Invalid JSON input.",
+      });
+    }
+
+    // Writing file to storage
+    fs.writeFile(file, data, (err) => {
+      if (err) {
+        console.error("Error saving file:", err);
+        return res.status(500).json({
+          error: "Error while storing the file into the storage.",
+        });
+      }
+
+      axios
+        .post("http://localhost:6000/calculate", {
+          file,
+          product: "wheat",
+        })
+        .then((response) => {
+          console.log("Axios request successful:", response.data);
+          res.status(201).json({ file: file, message: "Success." });
+        })
+        .catch((error) => {
+          console.error("Axios error:", error);
+          res.status(500).json({ error: "Error in external API request." });
+        });
+    });
+  } catch (err) {
+    console.error("Error in request:", err);
+    res.status(400).json({ error: "Failed to process request." });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
