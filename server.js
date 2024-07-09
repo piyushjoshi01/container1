e45a33c2f98fb1af59b8e6ae1718343d94e6be93
Piyush Rakeshbhai Joshi <py287300@dal.ca>
2024-07-09 19:04:04 -0300

    deploying container-1

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post("/store-file", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World from Container-1");
+  res.send("Hello World from Container-1 Kubernetes");
 });
 
 app.listen(port, () => {
