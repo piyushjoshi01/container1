f8a59eae173cec32ad53e250db2da69c00980f91
Piyush Rakeshbhai Joshi <py287300@dal.ca>
2024-07-09 22:41:47 -0300

    deploying container-1

--- server.js
+++ server.js
@@ -22,28 +22,34 @@ app.post("/store-file", async (req, res) => {
       if (err) {
         console.error("Error saving file:", err);
         return res.status(500).json({
-          error: "Error while storing the file into the storage.",
-        });
-      }
-
-      axios
-        .post("http://service-container2:80/calculate", {
           file,
-          product: "wheat",
-        })
-        .then((response) => {
-          console.log("Axios request successful:", response.data);
-          res.status(201).json({ file: file, message: "Success." });
-        })
-        .catch((error) => {
-          console.error("Axios error:", error);
-          res.status(500).json({ error: "Error in external API request." });
+          error: "Error while storing the file to the storage.",
         });
+      } else {
+        return res.status(201).json({ file, message: "Success." });
+      }
     });
   } catch (err) {
     console.error("Error in request:", err);
     res.status(400).json({ error: "Failed to process request." });
   }
+});
+
+app.post("/calculate", (req, res) => {
+  const { file, product } = req.body;
+  axios
+    .post("http://service-container2:80/calculate", {
+      file,
+      product,
+    })
+    .then((response) => {
+      console.log("Axios request successful:", response.data);
+      res.status(response.status).json(response.data);
+    })
+    .catch((error) => {
+      console.error("Axios error:", error);
+      res.status(500).json({ error: "Error in external API request." });
+    });
 });
 
 app.get("/", (req, res) => {
